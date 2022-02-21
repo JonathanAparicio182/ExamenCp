739badd43807812aebb167a8d487edddb047adab
jonathan <jonathanaparicio182@gmail.com>
2022-02-20 20:34:33 -0600

    Paginación manual para cargar más información

--- app/src/main/java/com/superhero/examencoppel/ui/viewmodels/HeroesVM.kt
+++ app/src/main/java/com/superhero/examencoppel/ui/viewmodels/HeroesVM.kt
@@ -2,6 +2,7 @@ package com.superhero.examencoppel.ui.viewmodels
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.map
 import androidx.lifecycle.viewModelScope
 import com.superhero.examencoppel.domain.GetHeroUseCase
 import com.superhero.examencoppel.domain.SuperHero
@@ -13,11 +14,22 @@ import javax.inject.Inject
 class HeroesVM @Inject constructor(private val getHeroUseCase: GetHeroUseCase): ViewModel() {
     val isLoading = MutableLiveData<Boolean>()
     val heroes = MutableLiveData<List<SuperHero>>()
+    private var offset = 0
     fun onCreate(){
         viewModelScope.launch {
             isLoading.postValue(true)
-            val result = getHeroUseCase(0)
+            val result = getHeroUseCase(offset)
             heroes.postValue(result)
+            isLoading.postValue(false)
+        }
+    }
+    fun loadMore(currentList: MutableList<SuperHero>){
+        viewModelScope.launch {
+            isLoading.postValue(true)
+            offset += 20
+            val result = getHeroUseCase(offset)
+            result.forEach { currentList.add(it) }
+            heroes.postValue(currentList)
             isLoading.postValue(false)
         }
     }
--- app/src/main/java/com/superhero/examencoppel/ui/views/HeroesFragment.kt
+++ app/src/main/java/com/superhero/examencoppel/ui/views/HeroesFragment.kt
@@ -5,10 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AbsListView
+import androidx.core.view.isVisible
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.superhero.examencoppel.R
 import com.superhero.examencoppel.databinding.FragmentHeroesBinding
 import com.superhero.examencoppel.domain.SuperHero
@@ -19,7 +22,10 @@ class HeroesFragment : Fragment() {
     private lateinit var binding: FragmentHeroesBinding
     private val viewModel: HeroesVM by activityViewModels()
     private lateinit var recyclerAdapter: HeroAdapter
-    private var listHero: List<SuperHero> = emptyList()
+    private var isScrolling = false
+    private var currentItems = 0
+    private var totalItems = 0
+    private var scrollOutItems = 0
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?): View {
         binding = DataBindingUtil.inflate(inflater,R.layout.fragment_heroes, container, false)
@@ -41,14 +47,38 @@ class HeroesFragment : Fragment() {
                 it.comics.toTypedArray()
             ))
         }
+        val manager = LinearLayoutManager(context)
         with(binding.rvHeroes){
-            layoutManager = LinearLayoutManager(context)
+            layoutManager = manager
             adapter = recyclerAdapter
+            addOnScrollListener(object : RecyclerView.OnScrollListener(){
+                override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                    super.onScrollStateChanged(recyclerView, newState)
+
+                    if (newState == AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL)
+                        isScrolling = true
+                }
+
+                override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                    super.onScrolled(recyclerView, dx, dy)
+
+                    currentItems = manager.childCount
+                    totalItems = manager.itemCount
+                    scrollOutItems = manager.findFirstVisibleItemPosition()
+                    if (isScrolling && (currentItems + scrollOutItems == totalItems)){
+                        isScrolling = false
+                        viewModel.loadMore(recyclerAdapter.currentList.toMutableList())
+                    }
+                }
+            })
         }
     }
     private fun setupObserver(){
         viewModel.heroes.observe(viewLifecycleOwner){
             recyclerAdapter.submitList(it)
+        }
+        viewModel.isLoading.observe(viewLifecycleOwner){
+            binding.pbLoading.isVisible = it
         }
     }
     private fun configFragment(){
